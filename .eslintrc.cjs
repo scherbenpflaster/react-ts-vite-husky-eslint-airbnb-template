3fec410c114087087a95a4bde0628fdee094b11f
Anna Kopylowicz <anna.kopylowicz@realcore.de>
2023-11-25 14:02:57 +0100

    chore: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,26 +2,28 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "airbnb",
-    "airbnb-typescript",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-type-checked", // @typescript-eslint @v6
-    "plugin:@typescript-eslint/stylistic-type-checked", // @typescript-eslint @v6
-    "plugin:react-hooks/recommended",
-    "plugin:react/jsx-runtime",
+    'airbnb',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked', // @typescript-eslint @v6
+    'plugin:@typescript-eslint/stylistic-type-checked', // @typescript-eslint @v6
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
   ],
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
   },
   root: true,
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
+    'comma-dangle': ['error', 'always-multiline'],
+    'prettier/prettier': ['error'],
   },
 };
